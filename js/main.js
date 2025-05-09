b0a18f3e6df3e05b1dbeb4a2bc231d2a04c19bb1
ValeriaM <144740424+ValNaMo@users.noreply.github.com>
2025-05-08 23:01:16 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://3.237.71.198:5000/api/devices';
+const API_URL = 'http://44.192.109.11:5000/api/devices';
 const tablaBody = document.getElementById('tablaDatos');
 const statusActual = document.getElementById('statusActual');
 
