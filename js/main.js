ce2c01942741bf9e711090058dd6702353991a9b
ValeriaM <144740424+ValNaMo@users.noreply.github.com>
2025-05-19 11:15:25 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://3.236.175.221:5000/api/devices';
+const API_URL = 'http://44.204.121.80:5000/api/devices';
 const tablaBody = document.getElementById('tablaDatos');
 const statusActual = document.getElementById('statusActual');
 
